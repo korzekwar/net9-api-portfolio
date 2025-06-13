f9c89612346b3761f4c84ffdbd4ba32299299da6
Alexsandra Mathias Moretto <ricardokk7@gmail.com>
2025-06-13 09:09:44 -0300

    docs: Add and configure Swagger for API documentation

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -8,9 +8,17 @@ builder.Services.AddControllers();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 using (var scope = app.Services.CreateScope())
 {
